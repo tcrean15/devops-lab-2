e9d464cee1af46efaa1bbd150b99986284174bb9
Thomas <C21416014@mytudublin.ie>
2023-10-04 15:27:28 +0100

    change greeting to welcome

--- helloworld.c
+++ helloworld.c
@@ -1,10 +1,9 @@
 #include <stdio.h>
 
 int main() {
-    // Use a for loop to iterate from 1 to 10
-    for (int i = 1; i <= 10; i++) {
-        printf("%d\n", i); // Print the current value of 'i'
-    }
+    
+        printf("Welcome");
+    
     
     return 0;
 }
